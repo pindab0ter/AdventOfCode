1d40e746029cbd2a7d919179a02f24a70575915c
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2025-12-01 23:46:38 +0100

    Solve 2025 day 1 part 2 with Kotlin

--- src/main/kotlin/nl/pindab0ter/aoc2025/day01/SecretEntrance.kt
+++ src/main/kotlin/nl/pindab0ter/aoc2025/day01/SecretEntrance.kt
@@ -1,13 +1,29 @@
 package nl.pindab0ter.aoc2025.day01
 
-import nl.pindab0ter.aoc2025.day01.Rotation.Direction
-import nl.pindab0ter.aoc2025.day01.Rotation.Direction.LEFT
-import nl.pindab0ter.aoc2025.day01.Rotation.Direction.RIGHT
+import nl.pindab0ter.aoc2025.day01.Instruction.Direction
+import nl.pindab0ter.aoc2025.day01.Instruction.Direction.LEFT
+import nl.pindab0ter.aoc2025.day01.Instruction.Direction.RIGHT
 import nl.pindab0ter.common.getInput
 import nl.pindab0ter.common.println
 
-data class Rotation(val direction: Direction, val steps: Int) {
-    override fun toString(): String = "$direction$steps"
+fun main() {
+    val instructions = getInput(2025, 1).parse()
+
+    val dialLog = followDirections(Dial(), instructions)
+
+    println("The amount of times the dial ended on 0 while following the directions: ${dialLog.count { it.position == 0 }}")
+    println("The amount of times the dial passed 0 while following the directions: ${dialLog.last().timesPointedToZero}")
+}
+
+fun String.parse(): List<Instruction> = lines().map {
+    Instruction(
+        direction = Direction.from(it.first()),
+        clicks = it.drop(1).toInt()
+    )
+}
+
+data class Instruction(val direction: Direction, val clicks: Int) {
+    override fun toString(): String = "$direction$clicks"
 
     enum class Direction {
         LEFT, RIGHT;
@@ -21,40 +37,41 @@ data class Rotation(val direction: Direction, val steps: Int) {
         }
 
         override fun toString(): String = when (this) {
-            LEFT -> "L"
-            RIGHT -> "R"
+            LEFT -> "↺"
+            RIGHT -> "↻"
         }
     }
 }
 
-data class Dial(val position: Int) {
-    fun turn(rotation: Rotation) = Dial(
-        when (rotation.direction) {
-            LEFT -> this.position - rotation.steps
-            RIGHT -> this.position + rotation.steps
-        }.mod(100)
-    )
-}
+class Dial(val position: Int = 50, val timesPointedToZero: Int = 0) {
+    fun turn(instruction: Instruction): Dial {
+        var currentPosition = position
+        var newTimesPointedToZero = timesPointedToZero
 
-fun main() {
-    val directions = getInput(2025, 1).parse()
+        repeat(instruction.clicks) {
+            when (instruction.direction) {
+                LEFT -> currentPosition--
+                RIGHT -> currentPosition++
+            }
 
-    val dialLog = followDirections(Dial(50), directions)
+            when (currentPosition) {
+                -1 -> currentPosition = 99
+                100 -> currentPosition = 0
+            }
 
-    println("The amount of times the dial ended on 0 while following the directions: ${dialLog.count { it.position == 0 }}")
-}
+            if (currentPosition == 0) newTimesPointedToZero++
+        }
 
-fun String.parse(): List<Rotation> = lines().map {
-    Rotation(
-        direction = Direction.from(it.first()),
-        steps = it.drop(1).toInt()
-    )
+        return Dial(currentPosition, newTimesPointedToZero)
+    }
+
+    override fun toString(): String = "Dial(position=${position}, timesPointedToZero=${timesPointedToZero})"
 }
 
 fun followDirections(
     startingPosition: Dial,
-    directions: List<Rotation>,
-) = directions.fold(listOf(startingPosition)) { acc: List<Dial>, rotation: Rotation ->
-    acc.plus(acc.last().turn(rotation))
+    directions: List<Instruction>,
+) = directions.fold(listOf(startingPosition)) { acc: List<Dial>, instruction: Instruction ->
+    acc.plus(acc.last().turn(instruction))
 }
 
--- src/test/kotlin/nl/pindab0ter/aoc2025/day01/SecretEntranceKtTest.kt
+++ src/test/kotlin/nl/pindab0ter/aoc2025/day01/SecretEntranceKtTest.kt
@@ -1,7 +1,7 @@
 package nl.pindab0ter.aoc2025.day01
 
-import nl.pindab0ter.aoc2025.day01.Rotation.Direction.LEFT
-import nl.pindab0ter.aoc2025.day01.Rotation.Direction.RIGHT
+import nl.pindab0ter.aoc2025.day01.Instruction.Direction.LEFT
+import nl.pindab0ter.aoc2025.day01.Instruction.Direction.RIGHT
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -13,12 +13,13 @@ import kotlin.test.assertEquals
 
 @DisplayName("2025 Day 01 - Secret Entrance")
 class SecretEntranceKtTest {
-    @ParameterizedTest(name = "{0} ⟲ {1} → {2}")
+    @ParameterizedTest(name = "{0} turn {1} → {2}")
     @MethodSource("turnDialProvider")
-    fun `Turn dial`(dial: Dial, rotation: Rotation, expected: Int) {
-        val actual = dial.turn(rotation).position
+    fun `Turn dial`(position: Int, instruction: Instruction, expectedPosition: Int, expectedTimesPointedToZero: Int) {
+        val turnedDial = Dial(position).turn(instruction)
 
-        assertEquals(expected, actual)
+        assertEquals(expectedPosition, turnedDial.position)
+        assertEquals(expectedTimesPointedToZero, turnedDial.timesPointedToZero)
     }
 
     @Test
@@ -35,37 +36,46 @@ class SecretEntranceKtTest {
             R14
             L82""".trimIndent().parse()
 
-        val dialLog = followDirections(Dial(50), directions);
+        val dialLog = followDirections(Dial(50), directions)
 
         val actual = dialLog.count { it.position == 0 }
 
         assertEquals(3, actual)
     }
 
-    @ParameterizedTest(name = "{1} → {0}")
-    @MethodSource("partTwoProvider")
-    fun `Part two`(expected: String, input: String) {
-        val actual = TODO()
+    @Test
+    fun `Part two`() {
+        val directions = """
+            L68
+            L30
+            R48
+            L5
+            R60
+            L55
+            L1
+            L99
+            R14
+            L82""".trimIndent().parse()
 
-        assertEquals(expected, actual)
+        val dialLog = followDirections(Dial(50), directions)
+
+        val actual = dialLog.last().timesPointedToZero
+
+        assertEquals(6, actual)
     }
 
     companion object {
         @JvmStatic
         fun turnDialProvider(): Stream<Arguments> = Stream.of(
-            arguments(Dial(11), Rotation(RIGHT, 8), 19),
-            arguments(Dial(19), Rotation(LEFT, 19), 0),
-            arguments(Dial(0), Rotation(LEFT, 1), 99),
-            arguments(Dial(99), Rotation(RIGHT, 1), 0),
-            arguments(Dial(50), Rotation(RIGHT, 200), 50),
-            arguments(Dial(50), Rotation(LEFT, 200), 50),
-            arguments(Dial(99), Rotation(RIGHT, 101), 0),
-            arguments(Dial(0), Rotation(LEFT, 101), 99),
-        )
-
-        @JvmStatic
-        fun partTwoProvider(): Stream<Arguments> = Stream.of(
-            arguments("expected", "actual"),
+            arguments(11, Instruction(RIGHT, 8), 19, 0),
+            arguments(19, Instruction(LEFT, 19), 0, 1),
+            arguments(0, Instruction(LEFT, 1), 99, 0),
+            arguments(99, Instruction(RIGHT, 1), 0, 1),
+            arguments(50, Instruction(RIGHT, 200), 50, 2),
+            arguments(50, Instruction(LEFT, 200), 50, 2),
+            arguments(99, Instruction(RIGHT, 101), 0, 2),
+            arguments(0, Instruction(LEFT, 101), 99, 1),
+            arguments(50, Instruction(RIGHT, 1000), 50, 10),
         )
     }
 }
