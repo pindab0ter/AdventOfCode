9740c41d541a4790e4abfa5b561d28d45d9d3655
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2025-12-07 10:34:50 +0100

    Solve 2025 day 4 part 2 with Kotlin

--- src/main/kotlin/nl/pindab0ter/aoc2025/day04/PrintingDepartment.kt
+++ src/main/kotlin/nl/pindab0ter/aoc2025/day04/PrintingDepartment.kt
@@ -5,17 +5,26 @@ import nl.pindab0ter.common.*
 fun main() {
     val grid = getInput(2025, 4).parse()
 
-    val accessibleRolls = grid.accessibleRollsMarked()
+    val accessibleRollCount = grid.accessibleRollsMarked().count { it == 'x' }
+    println("Amount of rolls of paper accessible by forklift on the first go: $accessibleRollCount")
 
-    println("Amount of rolls of paper accessible by forklift: ${grid.count { it == 'x' }}")
+    val accessibleRollsRemoved = grid.countRolls() - grid.removeAccessibleRolls().countRolls()
+    println("Total amount of accessible rolls of paper removed: $accessibleRollsRemoved")
 }
 
 fun String.parse(): Grid<Char> = lines().map(String::toList).toGrid()
 
+fun Iterable<Char>.countRolls() = count { it == '@' }
+
 fun Grid<Char>.accessibleRollsMarked(): Grid<Char> = mapIndexed { x, y, value ->
     when {
-        value == '@' && neighbours(x, y).count { it == '@' } < 4 -> 'x'
+        value == 'x' -> '.'
+        value == '@' && neighbours(x, y).countRolls() < 4 -> 'x'
         else -> value
     }
 }
 
+fun Grid<Char>.removeAccessibleRolls(): Grid<Char> = generateSequence(this) { it.accessibleRollsMarked() }
+    .zipWithNext()
+    .first { (previous, current) -> current.countRolls() == previous.countRolls() }
+    .second
--- src/test/kotlin/nl/pindab0ter/aoc2025/day04/PrintingDepartmentKtTest.kt
+++ src/test/kotlin/nl/pindab0ter/aoc2025/day04/PrintingDepartmentKtTest.kt
@@ -2,10 +2,6 @@ package nl.pindab0ter.aoc2025.day04
 
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.Arguments.arguments
-import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
 import kotlin.test.assertEquals
 
 @DisplayName("2025 Day 04 - Printing Department")
@@ -27,22 +23,30 @@ class PrintingDepartmentKtTest {
             .trimIndent()
             .parse()
             .accessibleRollsMarked()
+            .count { it == 'x' }
 
         assertEquals(13, actual)
     }
 
-    //    @ParameterizedTest(name = "{1} → {0}")
-    @MethodSource("partTwoProvider")
-    fun `Part two`(expected: String, input: String) {
-        val actual = TODO()
+    @Test
+    fun `Part two`() {
+        val input = """
+            ..@@.@@@@.
+            @@@.@.@.@@
+            @@@@@.@.@@
+            @.@@@@..@.
+            @@.@@@@.@@
+            .@@@@@@@.@
+            .@.@.@.@@@
+            @.@@@.@@@@
+            .@@@@@@@@.
+            @.@.@@@.@.
+            """
+            .trimIndent()
+            .parse()
 
-        assertEquals(expected, actual)
-    }
+        val actual = input.countRolls() - input.removeAccessibleRolls().countRolls()
 
-    companion object {
-        @JvmStatic
-        fun partTwoProvider(): Stream<Arguments> = Stream.of(
-            arguments("expected", "actual"),
-        )
+        assertEquals(43, actual)
     }
 }
